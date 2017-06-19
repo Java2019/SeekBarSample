772f2d2195b2cf9d6dfd4fd795c1c17af5e72a3a
java2019 <java_2019@mail.ru>
2017-06-19 10:41:19 +0300

    Test 1

--- app/src/main/java/myseekbar/seekbarsample/SeekBarActivity.java
+++ app/src/main/java/myseekbar/seekbarsample/SeekBarActivity.java
@@ -1,13 +1,38 @@
 package myseekbar.seekbarsample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class SeekBarActivity extends AppCompatActivity {
+public class SeekBarActivity extends Activity implements SeekBar.OnSeekBarChangeListener{
+
+    TextView mTextValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seek_bar);
+
+        final SeekBar seekBar = (SeekBar)findViewById(R.id.seek_bar);
+        seekBar.setOnSeekBarChangeListener(this);
+
+        mTextValue = (TextView) findViewById(R.id.text_value);
+        mTextValue.setText("0");
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        mTextValue.setText(String.valueOf(seekBar.getProgress()));
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
